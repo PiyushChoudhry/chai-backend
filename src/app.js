4b489a34912da79cf69e512a4f647a5a207b7c39
Piyush Choudhary <chwdhry.sahab@gmail.com>
2026-02-22 21:00:40 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -15,7 +15,16 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public")) // to store public assest on server like photo, file, fevicon that are not sensitive
 app.use(cookieParser())
 
+// routes import
+import userRouter from "./routes/user.routes.js"
 
 
+// routes declaration
+// app.get() // it is not used here as before we are writing routes and controllers then and there only, but now we segregated things, so for using router, we must have to use middleware (and it is used by "app.use()")
+
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
+// http://localhost:8000/api/v1/users/login // and if login is also there in "user.routes.js" like register then it will look like this
 
 export { app }
